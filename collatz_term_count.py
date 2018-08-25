b16dad23ba54af3ee03906728eef6521f670d707
Nitin Kakria <42174336+nitindotphysics@users.noreply.github.com>
2018-08-25 14:34:16 +0530

    Update collatz_term_count.py

--- collatz_term_count.py
+++ collatz_term_count.py
@@ -23,12 +23,7 @@ except ValueError:
 
 nextNumber = n    	# Both n and nextNumber equal initially. We start counting terms in collatz from the input integer & go down to 1.
 
-i = 0			# No. of times first while ('master while') runs varies with defferent input,
-			# so we add 'i' with variable, that couts terms in collatz for each integer, to constantly change variable with each 
-			# iteration of 'master while', see 'termsCollatz_i' below. 
-
 while nextNumber > 1:		# This 'master while' runs only for positive integers. 
-	i = i + 1		# Counts each iteration of 'master while'.
 	n = nextNumber 		# The 'nextNumber' from last line of 'master while' fed to n as now we need to do 2nd 'while' on that no.
 	step = 0
 	
@@ -36,8 +31,8 @@ while nextNumber > 1:		# This 'master while' runs only for positive integers.
 	while n > 1:		# Call collatz(n) again & again with its previous return value as new parameter. 
 		step = step + 1 # Counts no. of terms (steps) in collatz for integer n.
 		n = collatz(n)
-	termsCollatz_i = step	 
-	print(str(nextNumber) + ' has ' + str(termsCollatz_i) + ' no. of steps in its collatz')
+	termsCollatz = step	 
+	print(str(nextNumber) + ' has ' + str(termsCollatz) + ' no. of steps in its collatz')
 	nextNumber = nextNumber - 1 
 	
 if n < 1:		# This executes when user enters a negative integer.
